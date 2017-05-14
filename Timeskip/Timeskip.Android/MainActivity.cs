cffdb36bda504c04795ea7678b97d46508d05c77
Olivier Vanderstede <otje@telenet.be>
2017-05-15 00:18:23 +0200

    Disable Android hardware button when not logged in

--- Timeskip/Timeskip.Android/MainActivity.cs
+++ Timeskip/Timeskip.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Timeskip;
 
 namespace Timeskip.Droid
 {
@@ -21,6 +22,13 @@ namespace Timeskip.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        public override void OnBackPressed()
+        {
+            if (!App.LoggedIn) return;
+
+            base.OnBackPressed();
         }
     }
 }
